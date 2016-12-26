740e76ad5f094eca7eafef1f2da1fa88c3b98556
Hugo Fabricio <me@hugofabricio.com>
2016-12-26 11:57:05 -0300

    Adicionado bower

--- gulp/vendors.js
+++ gulp/vendors.js
@@ -4,7 +4,11 @@ import gulp from 'gulp';
 gulp.task('vendors', () => {
   return gulp.src([
     `${cons.vendor}/jquery/dist/jquery.js`,
-    `${cons.vendor}/bootstrap-sass/assets/javascripts/bootstrap.js`
+    `${cons.vendor}/bootstrap-sass/assets/javascripts/bootstrap.js`,
+    `${cons.vendor}/jquery-mask-plugin/dist/jquery.mask.js`,
+    `${cons.vendor}/retina.js/dist/retina.js`,
+    `${cons.vendor}/magnific-popup/dist/jquery.magnific-popup.js`,
+    `${cons.vendor}/Swiper/dist/js/swiper.jquery.js`
   ])
   .pipe(cons.$.newer(`${cons.tmp}/scripts`))
   .pipe(cons.$.sourcemaps.init())
